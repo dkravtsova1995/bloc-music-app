06d734488a8f2ab466a6dc2bcd878ff7e1151c0a
Daria Kravtsova <kravcova.dasha@bk.ru>
2020-02-04 15:00:00 -0700

    Updates

---
+++ precache-manifest.5b1314c5fa003ec21559fa3f5623503d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08ac065bcef30224bd55",
+    "url": "/bloc-music-app/static/js/runtime~main.d03f322b.js"
+  },
+  {
+    "revision": "9b57459d14a339b9a183",
+    "url": "/bloc-music-app/static/js/main.48786083.chunk.js"
+  },
+  {
+    "revision": "3d02afd4c62b62f2a70a",
+    "url": "/bloc-music-app/static/js/2.44d80114.chunk.js"
+  },
+  {
+    "revision": "9b57459d14a339b9a183",
+    "url": "/bloc-music-app/static/css/main.948713e6.chunk.css"
+  },
+  {
+    "revision": "ce8c490d5bac73c3eb18e848efd5ca4a",
+    "url": "/bloc-music-app/index.html"
+  }
+];
